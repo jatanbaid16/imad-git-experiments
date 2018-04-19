7614d54d76875a0c7c3fbef3b1ac781291bca4bd
Jatan <jatanbaid16@gmail.com>
2018-04-19 23:04:29 +0530

    Halfway done with my new feature.Tested so far so good!

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+function newfeature()
+{
+	console.log('Writing new code');
+	console.log('Halfway done');
+}
+
+
+
 function something(){
 console.log('I\'m something');
 }
 something();
+newfeature();
 
 
 console.log('hello world');
